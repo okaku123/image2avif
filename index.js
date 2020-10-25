647e41b594691cd303144a1efc58cbb92f229f5e
okaku123 <32980346+okaku123@users.noreply.github.com>
2020-10-25 21:26:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const github = require('@actions/github');
 
 
 const fs = require('fs')
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 const wasm_avif = require('@saschazar/wasm-avif');   
 // const wasm_mozjpeg = require('@saschazar/wasm-mozjpeg');
 const wasm_image_loader = require('@saschazar/wasm-image-loader');
@@ -27,24 +27,64 @@ const avifDefaultOptions  = require( '@saschazar/wasm-avif/options');
 
 // const fetchImage = async () => new Uint8Array(await fetch(SAMPLE_URL).then(res => res.buffer()));
 
+
+// (async function(){
+//   let info = await fetch("http://127.0.0.1:2020/task").then(res=>res.json())
+//   var { imagePath } = info
+//   var imageUrl = `http://127.0.0.1:2020${imagePath}`
+//   const fetchImage =  async () => new Uint8Array(await fetch(imageUrl).then(res => res.buffer()));
+//   let imageData = await fetchImage()
+//   console.log(imageData)
+// })()
+   
+
+const remoteUrl = "http://202.182.114.185:2020"
+// const remoteUrl = "http://127.0.0.1:2020"
+
+const loadNetJpeg = async () => {
+  let info = await fetch(`${remoteUrl}/task`).then(res=>res.json())
+  var { imagePath } = info
+  var imageUrl = `${remoteUrl}${imagePath}`
+  console.log(imageUrl)
+  const fetchImage =  async () => new Uint8Array(await fetch(imageUrl).then(res => res.buffer()));
+  let imageData = await fetchImage()
+  console.log(imageData.length)
+  return imageData
+}
+
+
+
 // const options = {
 //     ...avifDefaultOptions, // MozJPEG's options
 //     in_color_space: 9, // let encoder know, that we're dealing with RGBA input buffer
 // };
 
 
-const loadJpegFile = async () => {
-    let fileName = "avif-test-1920.jpg"
-    let filePath = `${__dirname}/${fileName}`
-    console.log(filePath )
-    if (fs.existsSync(filePath)){
-         let file = fs.readFileSync(filePath) 
-         return new Uint8Array(file)
-    }
+// const loadJpegFile = async () => {
+//     let fileName = "avif-test-1920.jpg"
+//     let filePath = `${__dirname}/${fileName}`
+//     console.log(filePath )
+//     if (fs.existsSync(filePath)){
+//          let file = fs.readFileSync(filePath) 
+//          return new Uint8Array(file)
+//     }
+// }
+
+
+async function postData(data){
+  let option =  {
+    method: 'post',
+    headers: {
+      "Content-length": data.length
+   },
+    body: data
+  }
+  let t =  await fetch(`${remoteUrl}/complete` , option ).then(res=>res.text())
 }
 
 
- Promise.all([ loadJpegFile() , wasm_image_loader() , wasm_avif() ])
+//  Promise.all([ loadJpegFile() , wasm_image_loader() , wasm_avif() ])
+Promise.all([ loadNetJpeg() , wasm_image_loader() , wasm_avif() ])
 .then(([buffer, imageLoader, avif ]) => {
     const rawRBG = imageLoader.decode(buffer, buffer.length, 3)
     const { height, width } = imageLoader.dimensions()
@@ -53,6 +93,7 @@ const loadJpegFile = async () => {
     // fs.writeFileSync(`${__dirname}/test-output.avif` , avifBuffer )
     console.log(avifBuffer)
     console.log(avifBuffer.length)
+    postData(avifBuffer)
     imageLoader.free()
     avif.free()
 })
